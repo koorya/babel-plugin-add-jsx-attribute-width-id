82f53e3a77e7aee11bc9551a0b84ee532273b271
Kirill Ivanov <puaktopblm@yandex.ru>
2021-02-12 14:14:13 +0700

    change functionality. now we can set arrow funct as value of attribute. JSON config is not possible now

--- README.md
+++ README.md
@@ -8,27 +8,37 @@ npm install --save-dev babel-plugin-add-jsx-attribute-width-id
 
 ## Usage
 
-**.babelrc**
 
-```json
-{
-  "plugins": [
-    [
-      "babel-plugin-add-jsx-attribute-width-id",
-      {
-        "elements": ["svg"],
-        "attributes": [
-          {
-            "name": "width",
-            "value": "200",
-            "spread": false,
-            "literal": false,
-            "position": "end"
-          }
-        ]
-      }
-    ]
-  ]
+**svgr config**
+
+```js
+jsx: {
+  babelConfig: {
+    plugins: [
+      [
+        "babel-plugin-replace-jsx-attribute-value-with-id",
+        {
+          ids: ["rect", "Y4"],
+          attributes: [
+            {
+              name: "onClick",
+              value: "props.clickfn",
+              spread: false,
+              literal: true,
+              position: "end",
+            },
+            {
+              name: "fill",
+              value: (id) => "props?." + id + ' ? "#0f0" : "#faa"',
+              spread: false,
+              literal: false,
+              position: "end",
+            },
+          ],
+        },
+      ],
+    ];
+  }
 }
 ```
 
--- src/index.js
+++ src/index.js
@@ -1,79 +1,115 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
 const positionMethod = {
   start: 'unshiftContainer',
-  end: 'pushContainer',
-}
+  end: 'pushContainer'
+};
 
-const addJSXAttribute = ({ types: t, template }, opts) => {
-  function getAttributeValue({ literal, value }) {
+const addJSXAttribute = ({
+  types: t,
+  template
+}, opts) => {
+  function getAttributeValue({
+    literal,
+    value,
+    id
+  }) {
     if (typeof value === 'boolean') {
-      return t.jsxExpressionContainer(t.booleanLiteral(value))
+      return t.jsxExpressionContainer(t.booleanLiteral(value));
     }
 
     if (typeof value === 'number') {
-      return t.jsxExpressionContainer(t.numericLiteral(value))
+      return t.jsxExpressionContainer(t.numericLiteral(value));
     }
 
     if (typeof value === 'string' && literal) {
-      return t.jsxExpressionContainer(template.ast(value).expression)
+      return t.jsxExpressionContainer(template.ast(value).expression);
     }
 
     if (typeof value === 'string') {
-      return t.stringLiteral(value)
+      return t.stringLiteral(value);
+    }
+    console.log("typeof value: "+typeof value);
+    if (typeof value === 'function') {
+      return t.jsxExpressionContainer(template.ast(value(id)).expression);
+      // return t.stringLiteral(value);
     }
 
-    return null
+    return null;
   }
 
-  function getAttribute({ spread, name, value, literal }) {
+  function getAttribute({
+    spread,
+    name,
+    value,
+    literal, id
+  }) {
     if (spread) {
-      return t.jsxSpreadAttribute(t.identifier(name))
+      return t.jsxSpreadAttribute(t.identifier(name));
     }
 
-    return t.jsxAttribute(
-      t.jsxIdentifier(name),
-      getAttributeValue({ value, literal }),
-    )
+    return t.jsxAttribute(t.jsxIdentifier(name), getAttributeValue({
+      value,
+      literal, id
+    }));
   }
 
   return {
     visitor: {
       JSXOpeningElement(path) {
-        if (!opts.elements.includes(path.node.name.name)) return
-
-        opts.attributes.forEach(
-          ({
+        let id = null;
+        if (!path.get('attributes').some(attribute => { //пропускаем элементы у которых id либо вообще нету, либо они не совпадают с id из массива
+          if(attribute.get('name').node.name != 'id') return false;
+          if(!opts.ids.includes(attribute.get('value').node.value) ) return false;
+          id = attribute.get('value').node.value;
+          return true;
+        }) ) return;
+        // if (!opts.ids.includes(path.node.name.name)) return;
+        opts.attributes.forEach(({
+          name,
+          value = null,
+          spread = false,
+          literal = false,
+          position = 'end'
+        }) => {
+          const method = positionMethod[position];
+          const newAttribute = getAttribute({
+            spread,
             name,
-            value = null,
-            spread = false,
-            literal = false,
-            position = 'end',
-          }) => {
-            const method = positionMethod[position]
-            const newAttribute = getAttribute({ spread, name, value, literal })
-            const attributes = path.get('attributes')
+            value,
+            literal, id
+          });
+          const attributes = path.get('attributes');
 
-            const isEqualAttribute = (attribute) => {
-              if (spread) {
-                return attribute.get('argument').isIdentifier({ name })
-              }
-
-              return attribute.get('name').isJSXIdentifier({ name })
+          const isEqualAttribute = attribute => {
+            if (spread) {
+              return attribute.get('argument').isIdentifier({
+                name
+              });
             }
 
-            const replaced = attributes.some((attribute) => {
-              if (!isEqualAttribute(attribute)) return false
-              attribute.replaceWith(newAttribute)
-              return true
-            })
+            return attribute.get('name').isJSXIdentifier({
+              name
+            });
+          };
 
-            if (!replaced) {
-              path[method]('attributes', newAttribute)
-            }
-          },
-        )
-      },
-    },
-  }
-}
+          const replaced = attributes.some(attribute => {
+            if (!isEqualAttribute(attribute)) return false;
+            attribute.replaceWith(newAttribute);
+            return true;
+          });
 
-export default addJSXAttribute
+          if (!replaced) {
+            path[method]('attributes', newAttribute);
+          }
+        });
+      }
+
+    }
+  };
+};
+
+var _default = addJSXAttribute;
+exports.default = _default;
